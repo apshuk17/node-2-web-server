adedfa442a7872c307a23636b810645531d4fcf1
Apoorva Shukla <apshuk17@gmail.com>
2017-01-28 16:43:26 +0530

    initial-commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express'),
+hbs = require('hbs'),
+app = express(),
+PORT = 3030;
+
+//Setting View engine
+app.set('view engine', 'hbs');
+
+//Registering Partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+//Registering getCurrentYear hbs helper
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+ //Register uppercase hbs helper
+ hbs.registerHelper('screamIt', (text) => {
+   return text.toUpperCase();
+ });
+
+ //use of middleware
+ app.use((req, res, next) => {
+   console.log(`Request method is: ${req.method} and request URL is: ${req.url}`);
+   next();
+ });
+
+//Setting static folder
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    message: 'Welcome to Home Page.'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Us Page',
+    message: 'Welcome to About Us Page.'
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
